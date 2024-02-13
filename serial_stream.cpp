68896be4ae098f17cd1cef84c2a78fe43421c278
elegresl <115160984+elegresl@users.noreply.github.com>
2024-02-13 16:40:25 -0400

    Update serial_stream.cpp

--- serial_stream.cpp
+++ serial_stream.cpp
@@ -15,7 +15,7 @@ int main()
 {
   using namespace std::this_thread; // sleep_for, sleep_until
   using namespace std::chrono;
-  using namespace std::unistd;
+  //using namespace std::unistd;
    SerialStream serial_stream;
 
    
@@ -29,9 +29,9 @@ int main()
    char read_byte_2 = 'B';
   
    serial_stream << 0xFF;
-  sleep(1);
+  unistd::sleep(1);
   serial_stream << 0xFF;
- sleep(1);
+ unistd::sleep(1);
 
   serial_stream << 0x00;
   sleep(1);
