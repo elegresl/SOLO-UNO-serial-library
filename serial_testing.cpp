a16d58ec8c79c186a6aabc7aa1a88f52783be291
elegresl <115160984+elegresl@users.noreply.github.com>
2024-02-18 18:37:36 -0400

    Update serial_testing.cpp

--- serial_testing.cpp
+++ serial_testing.cpp
@@ -9,46 +9,23 @@
 #include <sstream>
 
 constexpr const char* const SERIAL_PORT_2 = "/dev/ttyACM0" ;
-int main()
-{   
-    using namespace LibSerial;
-    SerialPort serial_port;
+void soloWrite(char addr, char cmd, int data){
  
-    try
-    {
-        serial_port.Open(SERIAL_PORT_2) ;
-    }
-    catch (const OpenFailed&)
-    {
-        std::cerr << "The serial port did not open correctly." << std::endl ;
-        return EXIT_FAILURE ;
-    }
- 
-    serial_port.FlushIOBuffers();
-    serial_port.FlushInputBuffer();
-    serial_port.FlushOutputBuffer();
-
-    serial_port.SetBaudRate(BaudRate::BAUD_115200) ;
-    serial_port.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
-    serial_port.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
-    serial_port.SetParity(Parity::PARITY_NONE) ; 
-    serial_port.SetStopBits(StopBits::STOP_BITS_1) ;
-
-    std::ofstream outputFile("serial_log.txt");
-
     char initiator = 0xFF;
-    char address = 0x00;
-    char command = 0x11;
-    char data0 = 0x00;
-    char data1 = 0x00;
-    char data2 = 0x00;
-    char data3 = 0x01;
+    char address = addr;
+    char command = cmd;
+   
+    char* bytes = reinterpret_cast<char*>(&hexValue);
+ 
+    char data0 = bytes[0];
+    char data1 = bytes[1];
+    char data2 = bytes[2];
+    char data3 = bytes[3];
+    
     char crc = 0x00;
     char ending = 0xFE;
         
     char data_byte[] = {initiator, initiator, address, command, data0, data1, data2, data3, crc, ending};
-       
-    std::this_thread::sleep_for(std::chrono::seconds(1));
     
     serial_port.WriteByte(data_byte[0]);
     serial_port.DrainWriteBuffer();
@@ -79,6 +56,44 @@ int main()
      
     serial_port.WriteByte(data_byte[9]);
     serial_port.DrainWriteBuffer() ;
+ 
+}
+
+boolean initSolo(){
+   using namespace LibSerial;
+    SerialPort serial_port;
+ 
+    try
+    {
+        serial_port.Open(SERIAL_PORT_2) ;
+    }
+    catch (const OpenFailed&)
+    {
+        std::cerr << "The serial port did not open correctly." << std::endl ;
+        return EXIT_FAILURE ;
+    }
+ 
+    serial_port.FlushIOBuffers();
+    serial_port.FlushInputBuffer();
+    serial_port.FlushOutputBuffer();
+
+    serial_port.SetBaudRate(BaudRate::BAUD_115200) ;
+    serial_port.SetCharacterSize(CharacterSize::CHAR_SIZE_8) ;
+    serial_port.SetFlowControl(FlowControl::FLOW_CONTROL_NONE) ;
+    serial_port.SetParity(Parity::PARITY_NONE) ; 
+    serial_port.SetStopBits(StopBits::STOP_BITS_1) ;
+
+    std::ofstream outputFile("serial_log.txt");
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+ 
+}
+int main()
+{   
+    
+
+    initSolo();
+    soloWrite();
 
     std::string reading;
  
